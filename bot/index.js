849bebf7779d74ed3b239400e153eb047ed8d3a2
vnikonov63 <nikonov.va@yandex.ru>
2020-08-26 18:08:02 +0300

    Release telegram bot with the database

--- bot/index.js
+++ bot/index.js
@@ -1,23 +1,34 @@
-const { Telegraf } = require('telegraf');
-const Student = require('../models/students');
-const mongoose = require('mongoose');
-const db = require('../seed');
-const bot = new Telegraf('1270977997:AAF5SnLOh3bnznqKiuMvoiu9JGPvpD3Ecyk');
+const { Telegraf } = require("telegraf");
+const Student = require("../models/students");
+const mongoose = require("mongoose");
+const bot = new Telegraf("986785690:AAH5awaR1tJANp-oGS4t_2vNJPJztioCkdI");
 
-async function today() {
-  console.log(user);
-  return user;
-}
+mongoose.connect("mongodb://localhost:27017/elbrusBirthday", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-bot.start((ctx) => ctx.reply('che prishel? idi naher!'));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on('sticker', (ctx) => ctx.reply('👍'));
-bot.on('text', async (ctx) => {
-  const user = await Student.findOne({ firstName: 'Artem' });
-  console.log(user);
-  ctx.reply(user);
+// async function today() {
+//   console.log(user);
+//   return user;
+// }
+
+bot.start((ctx) => ctx.reply("che prishel? idi naher!"));
+bot.help((ctx) => ctx.reply("Send me a sticker"));
+bot.on("sticker", (ctx) => ctx.reply("👍"));
+bot.on("text", async (ctx) => {
+  // let user = await Student.findOne({ firstName: "Artem" });
+  // console.log(user);
+  // async function a() {
+  //   let result = await Student.findOne({ firstName: "Artem" });
+  //   console.log(result);
+  //   return result;
+  // }
+  // const reult = await a();
+  // // await ctx.reply("123");
+  let result = await Student.findOne({ firstName: "Artem" });
+  await ctx.reply(result.firstName);
 });
 
 bot.launch();
-
 // bot.startPolling();
--- seed.js
+++ seed.js
@@ -1,21 +1,23 @@
-const mongoose = require('mongoose');
-let db = mongoose.connect('mongodb://localhost:27017/elbrusBirthday', {
+const mongoose = require("mongoose");
+let db = mongoose.connect("mongodb://localhost:27017/elbrusBirthday", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 // Just for the sake of checking whether the database is structured properly
-const { connection } = require('mongoose');
+const { connection } = require("mongoose");
 
-const Student = require('./models/students');
+const Student = require("./models/students");
 
 db.then(async ({ connection }) => {
   await connection.db.dropDatabase();
   await Student.create({
-    firstName: 'Artem',
-    lastName: 'Nikonov',
+    firstName: "Artem",
+    lastName: "Nikonov",
     dateOfBirth: new Date(Date.UTC(2000, 3, 27)),
     typeBootCamp: "online",
   });
   connection.close();
 });
+
+module.exports = db;
