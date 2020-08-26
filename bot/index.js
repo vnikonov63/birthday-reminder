6594a8bcf5d8cc5a476795f2fa60781e5d704975
ArtemK <brtoaster@gmail.com>
2020-08-26 17:28:56 +0300

    telegram-bot

--- bot/index.js
+++ bot/index.js
@@ -1,24 +1,23 @@
 const { Telegraf } = require('telegraf');
 const Student = require('../models/students');
-
+const mongoose = require('mongoose');
+const db = require('../seed');
 const bot = new Telegraf('1270977997:AAF5SnLOh3bnznqKiuMvoiu9JGPvpD3Ecyk');
 
-bot.start((ctx) => ctx.reply('che prishel? idi naher!'));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on('sticker', (ctx) => ctx.reply('👍'));
-bot.hears('today', async (ctx) => {
-  const user = await today();
-  ctx.reply(`${user}`);
-});
-
-bot.launch();
-
 async function today() {
-  const user = await Student.findOne({ firstName: 'Artem' });
   console.log(user);
   return user;
 }
 
+bot.start((ctx) => ctx.reply('che prishel? idi naher!'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.on('text', async (ctx) => {
+  const user = await Student.findOne({ firstName: 'Artem' });
+  console.log(user);
+  ctx.reply(user);
+});
 
+bot.launch();
 
 // bot.startPolling();
