12ad4e81b24c84116b0a53efcb5e56369a5b29b4
vnikonov63 <nikonov.va@yandex.ru>
2020-08-26 20:36:35 +0300

    Release Telegram bor send result

--- bot/index.js
+++ bot/index.js
@@ -3,29 +3,52 @@ const Student = require("../models/students");
 const mongoose = require("mongoose");
 const bot = new Telegraf("986785690:AAH5awaR1tJANp-oGS4t_2vNJPJztioCkdI");
 
+const { CronJob } = require("cron");
+
+// console.log("Before job instantiation");
+// const job = new CronJob("* * * * * *", function () {
+//   const d = new Date();
+//   console.log("Every second:", d);
+// });
+// console.log("After job instantiation");
+// job.start();
+
 mongoose.connect("mongodb://localhost:27017/elbrusBirthday", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// async function today() {
-//   console.log(user);
-//   return user;
+// async function returnPersonWithSuchDate() {
 // }
 
-bot.start((ctx) => ctx.reply("che prishel? idi naher!"));
+// bot.start((ctx) => ctx.reply("che prishel? idi naher!"));
 bot.help((ctx) => ctx.reply("Send me a sticker"));
+bot.start((ctx) => {
+  const job = new CronJob(
+    "30 34 20 * * *",
+    async function () {
+      let today = new Date().toDateString().split(" ");
+      [todayMonth, todayDay] = [today[1], today[2]];
+      let a = await Student.find();
+      let result = [];
+      a.forEach((student) => {
+        let studentDate = student.dateOfBirth.toDateString().split(" ");
+        [StudenttodayMonth, StudenttodayDay] = [studentDate[1], studentDate[2]];
+        if (todayMonth === StudenttodayMonth && todayDay === StudenttodayDay) {
+          ctx.reply(
+            `${student.firstName} ${student.lastName} has birthday today`
+          );
+        }
+      });
+    },
+    null,
+    true,
+    "Europe/Moscow"
+  );
+  job.start();
+});
 bot.on("sticker", (ctx) => ctx.reply("👍"));
 bot.on("text", async (ctx) => {
-  // let user = await Student.findOne({ firstName: "Artem" });
-  // console.log(user);
-  // async function a() {
-  //   let result = await Student.findOne({ firstName: "Artem" });
-  //   console.log(result);
-  //   return result;
-  // }
-  // const reult = await a();
-  // // await ctx.reply("123");
   let result = await Student.findOne({ firstName: "Artem" });
   await ctx.reply(result.firstName);
 });
