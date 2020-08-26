7863244de3d9391e70ce9f80b5879747f7a1163e
vnikonov63 <nikonov.va@yandex.ru>
2020-08-26 15:12:54 +0300

    Changed the name of the collection

--- models/students.js
+++ models/students.js
@@ -1,9 +1,9 @@
 const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema({
+const studentSchema = new Schema({
   firstName: String,
   lastName: String,
   dateOfBirth: Date,
 });
 
-module.exports = model("User", userSchema);
+module.exports = model("Student", studentSchema);
--- seed.js
+++ seed.js
@@ -7,11 +7,11 @@ let db = mongoose.connect("mongodb://localhost:27017/elbrusBirthday", {
 // Just for the sake of checking whether the database is structured properly
 const { connection } = require("mongoose");
 
-const User = require("./models/users");
+const Student = require("./models/students");
 
 db.then(async ({ connection }) => {
   await connection.db.dropDatabase();
-  await User.create({
+  await Student.create({
     firstName: "Vasilii",
     lastName: "Nikonov",
     dateOfBirth: new Date(Date.UTC(2000, 3, 27)),
