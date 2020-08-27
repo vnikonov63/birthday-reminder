3d9ffdc7e28df329ddb202a7674c64d3363a8a9b
vnikonov63 <nikonov.va@yandex.ru>
2020-08-27 21:31:38 +0300

    Release smth

--- bot/index.js
+++ bot/index.js
@@ -1,16 +1,16 @@
 function bot() {
-  const { Telegraf } = require('telegraf');
-  const Student = require('../models/students');
-  const mongoose = require('mongoose');
+  const { Telegraf } = require("telegraf");
+  const Student = require("../models/students");
+  const mongoose = require("mongoose");
   const bot = new Telegraf(process.env.BOT_TOKEN);
-  require('dotenv').config();
+  require("dotenv").config();
 
-  const { CronJob } = require('cron');
-  const { setMaxListeners } = require('../app');
+  const { CronJob } = require("cron");
+  const { setMaxListeners } = require("../app");
 
   let globalCtx;
   let jobMorning = new CronJob(
-    '35 * * * * *',
+    "35 * * * * *",
     async function () {
       let todayDate = new Date().toDateString().split(" ");
       console.log(todayDate);
@@ -24,26 +24,26 @@ function bot() {
               `У студента ${element.firstName} ${
                 element.lastName
               } сегодня день рождения. Локация: ${
-                element.typeBootCamp === 'moscow'
-                  ? 'Москва'
-                  : element.typeBootCamp === 'spb'
-                  ? 'Санкт-Петербург'
-                  : 'Онлайн'
+                element.typeBootCamp === "moscow"
+                  ? "Москва"
+                  : element.typeBootCamp === "spb"
+                  ? "Санкт-Петербург"
+                  : "Онлайн"
               }.`
             );
           });
         } else {
-          globalCtx.reply('Сегодня ни у кого нет дня рождения');
+          globalCtx.reply("Сегодня ни у кого нет дня рождения");
         }
       });
     },
     null,
     true,
-    'Europe/Moscow'
+    "Europe/Moscow"
   );
 
   let jobEvening = new CronJob(
-    '50 * * * * *',
+    "50 * * * * *",
     async function () {
       tomorrow().then((data) => {
         if (data.length) {
@@ -52,22 +52,22 @@ function bot() {
               `У студента ${element.firstName} ${
                 element.lastName
               } завтра день рождения. Локация: ${
-                element.typeBootCamp === 'moscow'
-                  ? 'Москва'
-                  : element.typeBootCamp === 'spb'
-                  ? 'Санкт-Петербург'
-                  : 'Онлайн'
+                element.typeBootCamp === "moscow"
+                  ? "Москва"
+                  : element.typeBootCamp === "spb"
+                  ? "Санкт-Петербург"
+                  : "Онлайн"
               }.`
             );
           });
         } else {
-          globalCtx.reply('Сегодня ни у кого нет дня рождения');
+          globalCtx.reply("Сегодня ни у кого нет дня рождения");
         }
       });
     },
     null,
     true,
-    'Europe/Moscow'
+    "Europe/Moscow"
   );
 
   mongoose.connect(process.env.MONGO_DB, {
@@ -76,41 +76,41 @@ function bot() {
   });
 
   const inlineKeyboardNotifications = Telegraf.Markup.inlineKeyboard([
-    Telegraf.Markup.callbackButton('Да 💃', 'notify'),
-    Telegraf.Markup.callbackButton('Нет 🙅‍♂️', 'nonotify'),
+    Telegraf.Markup.callbackButton("Да 💃", "notify"),
+    Telegraf.Markup.callbackButton("Нет 🙅‍♂️", "nonotify"),
   ]).extra();
 
-  bot.action('notify', async (ctx) => {
+  bot.action("notify", async (ctx) => {
     globalCtx = ctx;
-    await ctx.reply('Хорошо\n');
+    await ctx.reply("Хорошо\n");
     await ctx.reply(
-      'Теперь каждый день в 9 часов утра тебе будет приходить уведомление о днях рождениях сегодня'
+      "Теперь каждый день в 9 часов утра тебе будет приходить уведомление о днях рождениях сегодня"
     );
     await ctx.reply(
-      'Так же каждый день в 9 часов вечера тебе будет приходить уведомление о днях рождениях завтра'
+      "Так же каждый день в 9 часов вечера тебе будет приходить уведомление о днях рождениях завтра"
     );
     jobMorning.start();
     jobEvening.start();
   });
 
-  bot.action('nonotify', (ctx) => {
+  bot.action("nonotify", (ctx) => {
     globalCtx = ctx;
-    ctx.reply('Хорошо, мы не будем присылать тебе уведомления');
+    ctx.reply("Хорошо, мы не будем присылать тебе уведомления");
   });
 
   bot.start(async (ctx) => {
     await ctx.telegram.sendMessage(
       ctx.from.id,
-      'Привет, Маша! Ты хочешь получать уведомления?',
+      "Привет, Маша! Ты хочешь получать уведомления?",
       inlineKeyboardNotifications
     );
     await ctx.reply(
-      'Нажми /help для того, чтобы узнать про возможности нашего бота. Не забудь выбрать вариант уведомлений из предыдущего сообщения',
+      "Нажми /help для того, чтобы узнать про возможности нашего бота. Не забудь выбрать вариант уведомлений из предыдущего сообщения",
       Telegraf.Markup.keyboard([
-        ['Сегодня', 'Завтра'],
-        ['Остановить уведомления', 'Включить уведомления'],
-        ['Проверить включены ли уведомления'],
-        ['Ссылка на отправление формы ученикам'],
+        ["Сегодня", "Завтра"],
+        ["Остановить уведомления", "Включить уведомления"],
+        ["Проверить включены ли уведомления"],
+        ["Ссылка на отправление формы ученикам"],
       ])
         .oneTime()
         .resize()
@@ -118,26 +118,26 @@ function bot() {
     );
   });
 
-  bot.hears('Проверить включены ли уведомления', (ctx) => {
+  bot.hears("Проверить включены ли уведомления", (ctx) => {
     if (jobMorning.running === true) {
-      ctx.reply('Уведомления включены');
+      ctx.reply("Уведомления включены");
     } else {
-      ctx.reply('Уведомления выключены');
+      ctx.reply("Уведомления выключены");
     }
   });
 
-  bot.hears('Отправить ссылку на информацию студенту', (ctx) => {
-    ctx.reply('Button is working');
+  bot.hears("Отправить ссылку на информацию студенту", (ctx) => {
+    ctx.reply("Button is working");
   });
 
-  bot.hears('Остановить уведомления', (ctx) => {
+  bot.hears("Остановить уведомления", (ctx) => {
     if ((jobMorning.running === true) & (jobEvening.running === true)) {
       jobMorning.stop();
       jobEvening.stop();
     }
   });
 
-  bot.hears('Включить уведомления', (ctx) => {
+  bot.hears("Включить уведомления", (ctx) => {
     if ((jobMorning.running === false) & (jobEvening.running === false)) {
       jobMorning.start();
       jobEvening.start();
@@ -145,12 +145,12 @@ function bot() {
   });
 
   async function today() {
-    let today = new Date().toDateString().split(' ');
+    let today = new Date().toDateString().split(" ");
     [todayMonth, todayDay] = [today[1], today[2]];
     let students = await Student.find();
     let studentsToday = await Promise.all(
       students.filter((student) => {
-        let studentDate = student.dateOfBirth.toDateString().split(' ');
+        let studentDate = student.dateOfBirth.toDateString().split(" ");
         [StudenttodayMonth, StudenttodayDay] = [studentDate[1], studentDate[2]];
         return todayMonth === StudenttodayMonth && todayDay === StudenttodayDay;
       })
@@ -163,11 +163,11 @@ function bot() {
       Date.now() + 1000 * 60 * 60 * 24 + 1000 * 60 * 60 * 3
     )
       .toDateString()
-      .split(' ');
+      .split(" ");
     [tomorrowMonth, tomorrowDay] = [tomorrow[1], tomorrow[2]];
     let students = await Student.find();
     let studentsTomorrow = students.filter((student) => {
-      let studentDate = student.dateOfBirth.toDateString().split(' ');
+      let studentDate = student.dateOfBirth.toDateString().split(" ");
       [StudentTomorrowMonth, StudentTomorrowDay] = [
         studentDate[1],
         studentDate[2],
@@ -182,21 +182,21 @@ function bot() {
 
   bot.help((ctx) =>
     ctx.reply(
-      'Маша, привет! Этот телеграм бот создан для того' +
-        ' чтобы напоминать тебе о днях рождениях всех учеников' +
-        ' эльбруса!\nНажми today, чтобы узнать у кого день рождения' +
-        ' сегодня.\nНажми tomorrow, чтоб узнать у кого день рождения завтра \n' +
-        'Также один раз при регистрации необходимо написать комманду /start для того,' +
-        ' чтобы тебе приходили уведомления два раза в день'
+      "Маша, привет! Этот телеграм бот создан для того" +
+        " чтобы напоминать тебе о днях рождениях всех учеников" +
+        " эльбруса!\nНажми today, чтобы узнать у кого день рождения" +
+        " сегодня.\nНажми tomorrow, чтоб узнать у кого день рождения завтра \n" +
+        "Также один раз при регистрации необходимо написать комманду /start для того," +
+        " чтобы тебе приходили уведомления два раза в день"
     )
   );
 
-  bot.hears('Сегодня', async (ctx) => {
-    let today = new Date().toDateString().split(' ');
+  bot.hears("Сегодня", async (ctx) => {
+    let today = new Date().toDateString().split(" ");
     [todayMonth, todayDay] = [today[1], today[2]];
     let students = await Student.find();
     let studentsToday = students.filter((student) => {
-      let studentDate = student.dateOfBirth.toDateString().split(' ');
+      let studentDate = student.dateOfBirth.toDateString().split(" ");
       [StudenttodayMonth, StudenttodayDay] = [studentDate[1], studentDate[2]];
       return todayMonth === StudenttodayMonth && todayDay === StudenttodayDay;
     });
@@ -205,39 +205,30 @@ function bot() {
         ctx.reply(
           `У студента ${element.firstName} ${
             element.lastName
-<<<<<<< HEAD
-          } сегодня день рождения. Ему исполнилось 154 года. Локация: ${
+          } сегодня день рождения. Локация: ${
             element.typeBootCamp === "moscow"
               ? "Москва"
               : element.typeBootCamp === "spb"
               ? "Санкт-Петербург"
               : "Онлайн"
-=======
-          } сегодня день рождения. Локация: ${
-            element.typeBootCamp === 'moscow'
-              ? 'Москва'
-              : element.typeBootCamp === 'spb'
-              ? 'Санкт-Петербург'
-              : 'Онлайн'
->>>>>>> 6222f5e8120c1a7669874666bd4fccf5965c4acd
           }.`
         );
       });
     } else {
-      await ctx.reply('Сегодня ни у кого нет дня рождения');
+      await ctx.reply("Сегодня ни у кого нет дня рождения");
     }
   });
 
-  bot.hears('Завтра', async (ctx) => {
+  bot.hears("Завтра", async (ctx) => {
     let tomorrow = new Date(
       Date.now() + 1000 * 60 * 60 * 24 + 1000 * 60 * 60 * 3
     )
       .toDateString()
-      .split(' ');
+      .split(" ");
     [tomorrowMonth, tomorrowDay] = [tomorrow[1], tomorrow[2]];
     let students = await Student.find();
     let studentsTomorrow = students.filter((student) => {
-      let studentDate = student.dateOfBirth.toDateString().split(' ');
+      let studentDate = student.dateOfBirth.toDateString().split(" ");
       [StudentTomorrowMonth, StudentTomorrowDay] = [
         studentDate[1],
         studentDate[2],
@@ -252,30 +243,21 @@ function bot() {
         ctx.reply(
           `У студента ${element.firstName} ${
             element.lastName
-<<<<<<< HEAD
-          } Завтра день рождения \n Ему исполнилось 154 года. Локация: ${
+          } Завтра день рождения \n Локация: ${
             element.typeBootCamp === "moscow"
               ? "Москва"
               : element.typeBootCamp === "spb"
               ? "Санкт-Петербург"
               : "Онлайн"
-=======
-          } Завтра день рождения \n Локация: ${
-            element.typeBootCamp === 'moscow'
-              ? 'Москва'
-              : element.typeBootCamp === 'spb'
-              ? 'Санкт-Петербург'
-              : 'Онлайн'
->>>>>>> 6222f5e8120c1a7669874666bd4fccf5965c4acd
           }.`
         );
       });
     } else {
-      await ctx.reply('Завтра ни у кого нет дня рождения');
+      await ctx.reply("Завтра ни у кого нет дня рождения");
     }
   });
 
-  bot.on('sticker', (ctx) => ctx.reply('Очень классный стикер'));
+  bot.on("sticker", (ctx) => ctx.reply("Очень классный стикер"));
 
   bot.launch();
 }
