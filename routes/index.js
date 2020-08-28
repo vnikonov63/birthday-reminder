c6ea2a89040548ce71f08d69d92a182391e9856c
vnikonov63 <nikonov.va@yandex.ru>
2020-08-28 11:55:35 +0300

    Release added the sort of the students by name lastname upcomming birthday and default

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 // adding the necessary elements to work with the dates
-const dayjs = require('dayjs');
-let utc = require('dayjs/plugin/utc');
+const dayjs = require("dayjs");
+let utc = require("dayjs/plugin/utc");
 dayjs.extend(utc);
 
-const Student = require('../models/students');
+const Student = require("../models/students");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('index');
+router.get("/", (req, res) => {
+  res.render("index");
 });
 
-router.post('/', async (req, res) => {
-  if (typeof req.session.submit === 'undefined') {
+router.post("/", async (req, res) => {
+  if (typeof req.session.submit === "undefined") {
     const { name, lastname, birthday, city } = req.body;
-    req.session.submit = 'submit';
+    req.session.submit = "submit";
     const birthdayDate = new Date(dayjs.utc(birthday).format());
     let student = await new Student({
       firstName: name,
@@ -27,15 +27,15 @@ router.post('/', async (req, res) => {
       typeBootCamp: city,
       prettyDate: birthday,
     }).save();
-    res.redirect('/success');
+    res.redirect("/success");
   } else {
     const fail = true;
-    res.render('index', { fail });
+    res.render("index", { fail });
   }
 });
 
-router.get('/success', (req, res) => {
-  res.render('success');
+router.get("/success", (req, res) => {
+  res.render("success");
 });
 
 module.exports = router;
