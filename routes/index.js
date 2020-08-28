ded6ebbd9d770ab4526f7005d41d2fed7516918c
ArtemK <brtoaster@gmail.com>
2020-08-28 14:25:09 +0300

    Success page

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,19 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 // adding the necessary elements to work with the dates
-const dayjs = require("dayjs");
-let utc = require("dayjs/plugin/utc");
+const dayjs = require('dayjs');
+let utc = require('dayjs/plugin/utc');
 dayjs.extend(utc);
 
-const Student = require("../models/students");
+const Student = require('../models/students');
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("index");
+router.get('/', (req, res) => {
+  res.render('index');
 });
-
 
 router.post('/', async (req, res) => {
   const fail = true;
@@ -32,17 +31,13 @@ router.post('/', async (req, res) => {
         prettyDate: birthday,
         groupName: animal,
       }).save();
-      res.redirect('/success');
+      res.render('success', { student });
     } else {
       res.render('index', { invalidYear, student: req.body });
     }
   } else {
     res.render('index', { fail });
   }
-});
-
-router.get("/success", (req, res) => {
-  res.render("success");
 });
 
 function isOldEnough(date) {
